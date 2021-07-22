8c4560497d85260f28314cd1481d622504a1f636
Usarneme <jamestcoop@gmail.com>
2021-07-21 18:08:08 -0700

    :construction: WIP - some functionality works but not everything yet

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Create-Django-Blog-Site
+
 Script for generating a Python+Django blogging site; includes boilerplate folders and files for site administration, venv, and blog posts CRUD.
+
+--
+
+# NOTE: This doesn't work currently. Please do not use.
--- setup_script.sh
+++ setup_script.sh
@@ -9,6 +9,7 @@ then
   echo "CREATING DJANGO POWERED BLOG PROJECT: $NAME (NOTE: 21 STEPS TOTAL)"
   echo "1. Setting up virtual environment"
   python3 -m venv $NAME
+  source $NAME/bin/activate
   echo "1. Done"
 
   echo "2. Creating requirements.txt file for project dependencies"
@@ -43,15 +44,15 @@ then
   python manage.py migrate
   echo "8. Done"
 
-  echo "9. Creating ${$NAME} project"
+  echo "9. Creating $NAME project"
   python manage.py startapp $NAME
   echo "9. Done"
 
   echo "10. Updating Installed Apps in mysite/settings.py"
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then # Linux
-    sed -i "s/'django.contrib.staticfiles',/'django.contrib.staticfiles','blog.apps.BlogConfig',/g" mysite/settings.py
+    sed -i "s/'django.contrib.staticfiles',/'django.contrib.staticfiles','$NAME.apps.BlogConfig',/g" mysite/settings.py
   elif [[ "$OSTYPE" == "darwin"* ]]; then # Mac OSX
-    sed -i '' "s/'django.contrib.staticfiles',/'django.contrib.staticfiles','blog.apps.BlogConfig',/g" mysite/settings.py
+    sed -i '' "s/'django.contrib.staticfiles',/'django.contrib.staticfiles','$NAME.apps.BlogConfig',/g" mysite/settings.py
   fi
   echo "10. Done"
 
@@ -97,7 +98,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('$NAME.urls')),
 ]" >> mysite/urls.py
   echo "14. Done"
 
@@ -116,21 +117,21 @@ urlpatterns = [
 
 <head>
   <title>Blog Home</title>
-  <link rel="stylesheet" href="{% static 'css/blog.css' %}">
+  <link rel=\"stylesheet\" href=\"{% static 'css/blog.css' %}\">
 </head>
 
 <body>
-  <header class="page-header">
-    <div class="container">
+  <header class=\"page-header\">
+    <div class=\"container\">
       <nav>
         {% if user.is_authenticated %}
-          <a href="{% url 'post_new' %}" class="top-menu">
+          <a href=\"{% url 'post_new' %}\" class=\"top-menu\">
             {% include './icons/file-plus.svg' %} Add Post
           </a>
         {% endif %}
-        <a href="/admin" class="top-menu">Admin</a>
+        <a href=\"/admin\" class=\"top-menu\">Admin</a>
       </nav>
-      <h1><a href="/">Django Blog</a></h1>
+      <h1><a href=\"/\">Django Blog</a></h1>
     </div>
   </header>
   <main>
@@ -142,16 +143,16 @@ urlpatterns = [
   echo "{% extends 'blog/base.html' %}
 
 {% block content %}
-<article class="post">
-  <aside class="actions">
+<article class=\"post\">
+  <aside class=\"actions\">
     {% if user.is_authenticated %}
-      <a class="btn btn-default" href="{% url 'post_edit' pk=post.pk %}">
+      <a class=\"btn btn-default\" href=\"{% url 'post_edit' pk=post.pk %}\">
         {% include './icons/pencil.svg' %} Edit Post
       </a>
     {% endif %}
   </aside>
   {% if post.published_date %}
-  <time class="date">
+  <time class=\"date\">
     {{ post.published_date }}
   </time>
   {% endif %}
@@ -163,18 +164,18 @@ urlpatterns = [
 
 {% block content %}
 <h2>New post</h2>
-<form method="POST" class="post-form">{% csrf_token %}
+<form method=\"POST\" class=\"post-form\">{% csrf_token %}
   {{ form.as_p }}
-  <button type="submit" class="save btn btn-default">Save</button>
+  <button type=\"submit\" class=\"save btn btn-default\">Save</button>
 </form>
 {% endblock %}" >> $NAME/templates/blog/post_edit.html
   echo "{% extends 'blog/base.html' %}
 
 {% block content %}
   {% for post in posts %}
-    <article style="border: 2px solid slategray; margin: 22px; padding: 11px; background: rgba(255,255,255,0.6);">
+    <article style=\"border: 2px solid slategray; margin: 22px; padding: 11px; background: rgba(255,255,255,0.6);\">
       <time>published: {{ post.published_date }}</time>
-      <h2><a href="{% url 'post_detail' pk=post.pk %}">{{ post.title }}</a></h2>
+      <h2><a href=\"{% url 'post_detail' pk=post.pk %}\">{{ post.title }}</a></h2>
       <p>{{ post.text|linebreaksbr }}</p>
     </article>
   {% endfor %}
@@ -225,7 +226,7 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    if request.method == "POST":
+    if request.method == \"POST\":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -240,7 +241,7 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+    if request.method == \"POST\":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
@@ -255,12 +256,12 @@ def post_edit(request, pk):
 
   echo "20. Creating icons folder and svg files"
   mkdir $NAME/templates/blog/icons
-  echo "<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
-  <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
-  <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
+  echo "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-file-earmark-plus\" viewBox=\"0 0 16 16\">
+  <path d=\"M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z\"/>
+  <path d=\"M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z\"/>
 </svg>" >> $NAME/templates/blog/icons/file-plus.svg
-  echo "<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+  echo "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-pencil-fill\" viewBox=\"0 0 16 16\">
+  <path d=\"M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z\"/>
 </svg>" >> $NAME/templates/blog/icons/pencil.svg
   echo "20. Done"
 
@@ -270,7 +271,9 @@ def post_edit(request, pk):
 
   echo "ALL DONE. EXITING."
   echo
-  echo "To start the project, run ~ python manage.py runserver"
+  echo "To start the project"
+  echo "first run ~ source $NAME/bin/activate"
+  echo "then run ~ python manage.py runserver"
   echo "Then open your browser to localhost:8000"
 
 else
